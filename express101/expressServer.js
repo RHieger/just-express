b3b9d6d377e040f19edfab96c215233e825bbb64
Robert Hieger <rhieger@gmail.com>
2019-04-22 22:59:01 -0400

    Completed Lecture 12: Expres Server.
    Built rudimentary Express server with no routing capabilities whatsoever.

--- express101/expressServer.js
+++ express101/expressServer.js
@@ -9,3 +9,24 @@
 // Import Express module:
 
 const express = require('express');
+
+// Instantiate an instance of Express: expose the createApplication() function.
+
+const app = express();
+
+// all() is a method that takes two arguments as follows:
+//
+// 1.  The route
+// 2.  The callback function to run if the route is requested.
+
+app.all('*', (request, response) =>   {
+
+    // Express handles the basic  headers for the request.
+
+    response.send('<h1>This  is the home page!</h1>');
+
+});
+
+app.listen(3000);
+
+console.log('The server is listening on port 3000...');
