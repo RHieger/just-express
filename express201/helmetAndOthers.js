28de9228d00f231c121b0fe301049b8da8981029
Robert Hieger <rhieger@gmail.com>
2019-04-26 10:45:21 -0400

    Completed Lecture 16: Using helmet, etc.
    Explored use of third party middleware, helmet and other critical middleware native to Express.

--- express201/helmetAndOthers.js
+++ express201/helmetAndOthers.js
@@ -6,4 +6,50 @@
 //
 // Robert Hieger
 
+// Import Express module:
 
+const express = require('express');
+
+//  Invoke Express into app object:
+
+const app = express();
+
+// Import helmet to address basic HTTP request security concerns:
+
+const helmet = require('helmet');
+
+// Support helemt-based security:
+
+app.use( helmet() );
+
+// Set up public folder for static content:
+
+app.use( express.static('public') );
+
+// Support responses with JSON payload:
+
+app.use( express.json() );
+
+// Support URL-encoded payloads:
+
+app.use( express.urlencoded( {extended: false} ) );
+
+// NOTE: Most usually, the top three above use statementss will need to appear
+//       in the creation of any HTTP request. They allow for 1) serving
+//       up static content, 2) passsing the data as a JSON object (the
+//       de facto standard), and 3) transforming that object to URL
+//       encoded data that the browser can use for generation of content.
+
+app.post( '/ajax', (request, response) => {
+
+    // FOR DIAGNOSTIC PURPOSES ONLY:
+
+    console.log(request.body);
+
+    response.send('Test!');
+
+});
+
+app.listen(3000);
+
+console.log('Server listening on port 3000...');
