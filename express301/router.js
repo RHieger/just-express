46941c3e2bec7925264a0a13137aa2e83fe06913
Robert Hieger <rhieger@gmail.com>
2019-05-05 01:36:00 -0400

    Refreshed local wiki.

--- express301/router.js
+++ express301/router.js
@@ -5,3 +5,27 @@
 // Robert Hieger
 // May 5, 2019
 
+// Import Express module:
+
+const express = require('express');
+
+//  Instantiate an object of type express:
+
+const app = express();
+
+// Import third party helmet module:
+
+const  helmet = require('helmet');
+
+// SET UP MIDDLEWARE:
+
+app.use( helmet() );
+
+app.use( express.urlencoded() );
+
+app.use( express.json() );
+
+app.use( express.static('public') );
+
+// END MIDDLEWARE =====================
+
