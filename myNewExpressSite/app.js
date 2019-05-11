ab7a83b437ae8e558f3b91c5cdda92e93c51d7e2
Robert Hieger <rhieger@gmail.com>
2019-05-11 01:39:37 -0400

    Lecture 30 completed.
    Explored basic setup created by the Express Generator.

--- myNewExpressSite/app.js
+++ myNewExpressSite/app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Middleware Setup:
+
+const helmet = require('helmet');
+
+app.use( helmet() );
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
