d5dc67348c8ecdd5bc4bfb9307b0d8d3d36f9c92
Robert Hieger <rhieger@gmail.com>
2019-07-15 01:14:47 -0400

    Lecture 35: Made basic API configuration.

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// Set up API Key:
+
+const apiKey = 'd1923f949a5469388df50e0f61d9bdfb';
+
+// Configure API:
+
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { });
