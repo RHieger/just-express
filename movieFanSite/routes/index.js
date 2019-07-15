9b18c04b8acf44b9524ecd1204f9e8fc4ac4b9e6
Robert Hieger <rhieger@gmail.com>
2019-07-15 01:48:53 -0400

    Completed Lecture 36: Tested return of parsed  JSON data.

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// Import request module to support HTTP requests:
+
+const request = require('request');
+
 // Set up API Key:
 
 const apiKey = 'd1923f949a5469388df50e0f61d9bdfb';
@@ -15,7 +19,39 @@ const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  // Send HTTP request:
+
+  // request takes 2 arguments:
+  //   1. It takes the URL for the get operation.
+  //   2. The callback, which takes 3 arguments, to run on completion of the HTTP request:
+  //     a. error (if any)
+  //     b. http response
+  //     c. JSON data the server sent back
+
+  request.get(nowPlayingUrl, (error, response,  movieData) => {
+
+    // FOR DIAGNOSTIC PURPOSES ONLY:
+
+    // console.log("======The Error======");
+    // console.log(error);
+    // console.log("======The Response======");
+    // console.log(response);
+
+    // ACTIVE CODE:
+
+    const parsedData = JSON.parse(movieData);
+
+    // FOR DIAGNOSTIC PURPOSES ONLY:
+
+    console.log(parsedData);
+
+
+  });
+
   res.render('index', { });
+
+
 });
 
 module.exports = router;
