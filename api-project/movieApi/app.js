5f9f5737cdc47d851a26f14ed17a0054a350f07b
Robert Hieger <rhieger@gmail.com>
2019-08-07 22:49:18 -0400

    Lecture 40: Configure helmet.

--- api-project/movieApi/app.js
+++ api-project/movieApi/app.js
@@ -4,10 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const helmet = require('helmet');   // Security for movieApi
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use( helmet() );  // Invoke helmet()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
